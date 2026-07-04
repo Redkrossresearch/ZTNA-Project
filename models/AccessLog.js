5b419bdbd393c803d9765ee5ea03a150d90354ff
Aayush <aayushmansukh1703@gmail.com>
2026-07-04 14:50:07 +0530

    Enhanced device fingerprinting and access logging

--- models/AccessLog.js
+++ models/AccessLog.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
 const accessLogSchema = new mongoose.Schema({
-  userId: String,
-  ip: String,
-  device: String
+
+    userId: String,
+
+    ip: String,
+
+    browser: String,
+
+    os: String,
+
+    device: String,
+
+    deviceType: String
+
 }, {
-  timestamps: true   // 🔥 IMPORTANT (adds createdAt automatically)
+
+    timestamps: true
+
 });
 
 module.exports = mongoose.model("AccessLog", accessLogSchema);
